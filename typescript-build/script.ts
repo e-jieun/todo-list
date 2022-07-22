3200bab9cea09170361f3025e1e79a7fe0cd8922
jieunlee <liz950827@gmail.com>
2022-07-22 15:59:14 +0900

    file reset

---
+++ typescript-build/script.ts
@@ -0,0 +1,6 @@
+const root = document.getElementById('root');
+const caCon = document.getElementById('ca-con');
+const caBtn = document.getElementById('ca-plus');
+console.log(root);
+console.log(caCon);
+console.log(caBtn);
